12a36a0b985a2edd4550a8c9da385d2d943e82bb
Rahul Shiroshe <121466932+RahulShiroshe@users.noreply.github.com>
2024-01-08 16:02:26 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -28,21 +28,19 @@ function Contact() {
             <div className="content">
                 <h3 className="title">Contact info</h3>
                 <div className="info">
-                    <h3><i className="fa fa-envelope"></i> rahulphiroshe@gmail.com</h3>
-                    <h3><i className="fa fa-phone"></i> (+91) 79722 05933</h3>
-                    <h3><i className="fa fa-phone"></i> (+91) 94226 85155</h3>
-                    <h3><i className="fa fa-map-marker-alt"></i> 70 Washington Square South, New York, NY 10012, United States</h3>
+                    <h3><i className="fa fa-envelope"></i> support@nexusmart.com</h3>
+                    <h3><i className="fa fa-phone"></i> (+91) 022 230 84259</h3>
+                    <h3><i className="fa fa-phone"></i> (+91) 022 258 94664</h3>
+                    <h3><i className="fa fa-map-marker-alt"></i>  412, 3, Navjivan Soc, Lamington, Mumbai, Maharashtra 400008</h3>
                 </div>
             </div>
 
             <form id="myform" ref={formRef} onSubmit={handleSubmit} >
-                <input type="text" minlength="4" maxlength="20" id="user-name" placeholder="name" className="box" required />
-                <input type="email" id="user-email" placeholder="email" className="box" required />
-                <input type="text" minlength="3" maxlength="20" id="user-subject" placeholder="subject" className="box" required />
-                <textarea id="messege" cols="30" rows="10" placeholder="Messege" className="box messege" required></textarea>
-                {/* <button className="c-btn btn" type="submit" >Send <i className="fa fa-paper-plane"></i></button> */}
+                <input type="text" minlength="4" maxlength="20" id="user-name" placeholder="name" className="c-box" required />
+                <input type="email" id="user-email" placeholder="email" className="c-box" required />
+                <input type="text" minlength="3" maxlength="20" id="user-subject" placeholder="subject" className="c-box" required />
+                <textarea id="messege" cols="30" rows="10" placeholder="Messege" className="c-box messege" required></textarea>
                 <button className="c-btn btn" >Submit <i className="fa fa-paper-plane"></i></button>
-                {/* <button className="c-btn btn" type="submit" onSubmit={()=>sendMsg()} >Send <i className="fa fa-paper-plane"></i></button> */}
                 <div id="f-root"></div>
             </form>
         </div>
