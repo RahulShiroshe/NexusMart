96821c16d87fbc9c68b1175ddb82955170afc04d
Rahul Shiroshe <121466932+RahulShiroshe@users.noreply.github.com>
2024-01-08 15:54:48 +0530

    Update Footer.jsx

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -6,18 +6,18 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className='container grid2'>
+        <div className='container'>
           <div className='box p-l-t'>
             <a href='/'>
-                 <h1>Bonik</h1>
+                 <h1>NexusMart</h1>
             </a>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
-            <div className='icon d_flex'>
-              <div className='img d_flex'>
+            <div className='icon'>
+              <div className='img'>
                 <i className='fa-brands fa-google-play'></i>
                 <span>Google Play</span>
               </div>
-              <div className='img d_flex'>
+              <div className='img'>
                 <i className='fa-brands fa-app-store-ios'></i>
                 <span>App Store</span>
               </div>
@@ -65,10 +65,10 @@ const Footer = () => {
           <div className='box p-l-t'>
             <h2>Contact Us</h2>
             <ul>
-              <li className='footer-info'>70 Washington Square South, New York, NY 10012, United States </li>
-              <li className='footer-info'>Email: rahulpshiroshe@gmail.com</li>
-              <li className='footer-info'>Phone: +91 79722 05933</li>
-              <li className='footer-info'>Phone: +91 94226 85155</li>
+              <li className='footer-info'>412, 3, Navjivan Soc, Lamington, Mumbai, Maharashtra 400008 </li>
+              <li className='footer-info'>Email: support@nexusmart.com</li>
+              <li className='footer-info'>Phone: +91 022 230 84259</li>
+              <li className='footer-info'>Phone: +91 022 2589 4664</li>
             </ul>
           </div>
         </div>
