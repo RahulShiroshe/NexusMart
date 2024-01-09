7815484872a1964fbe55c5799c5636771330b682
Rahul Shiroshe <121466932+RahulShiroshe@users.noreply.github.com>
2024-01-10 00:20:11 +0530

    Update Head.jsx

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -3,7 +3,7 @@ import logo from "../../components/assets/images/logo.svg";
 
 const Head = () => {
   return (
-      <section className='head css-900'>
+      <section className='head s-head'>
         <div className='container-h'>
           <div className='left '>
             <div className='logo'>
@@ -12,13 +12,13 @@ const Head = () => {
               </a>
             </div>
             <i className='fa fa-phone'/>
-            <label className='s-900'> +91 01234 56789</label>
+            <label className='h-info'> +91 022 230 84259</label>
             <i className='fa fa-envelope'/>
-            <label className='s-900'> support@ui-lib.com</label>
+            <label className='h-info'> support@nexusmart.com</label>
           </div>
           <div className='right RText'>
-            <label className='s-900'>Theme FAQ"s</label>
-            <label className='s-900'>Need Helps</label>
+            <label className='h-info'>Theme FAQ"s</label>
+            <label className='h-info'>Need Helps</label>
             <i className="fa fa-flag" aria-hidden="true" style={{fontSize: "0.75rem"}}/>
             <label>EN</label>
               <i className="fa fa-flag" aria-hidden="true" style={{fontSize: "0.75rem"}}/>
